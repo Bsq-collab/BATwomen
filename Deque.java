f6e35212e91809595163c340d89bbb33585efeb4
Alison Lee <alee17@stuy.edu>
2017-03-31 10:44:37 -0400

    interface

---
+++ Deque.java
@@ -0,0 +1,8 @@
+public interface Deque<T> {
+    public void enqueueFirst() ;
+    public void enqueueLast() ;
+    public T dequeueFirst() ;
+    public T dequeueLast() ;
+    public T peekFirst() ;
+    public T peekLast() ;
+}
