3ae21e7f9d832c2b5801a0039e4a62305a4c9b2d
Alison Lee <alee17@stuy.edu>
2017-03-31 10:46:55 -0400

    commented out stuff

--- Deque.java
+++ Deque.java
@@ -1,8 +1,10 @@
 public interface Deque<T> {
+    /*
     public void enqueueFirst() ;
     public void enqueueLast() ;
     public T dequeueFirst() ;
     public T dequeueLast() ;
     public T peekFirst() ;
     public T peekLast() ;
+    */
 }
