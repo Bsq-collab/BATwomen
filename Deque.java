4ae4e823a28b7e75f5abab26a696e73b246b43cc
tructdao <tdao@stuy.edu>
2017-04-03 10:59:32 -0400

    fixed comments

--- Deque.java
+++ Deque.java
@@ -1,24 +1,24 @@
 public interface Deque<T> {
     
-    //adds an enQVal to the front of the queue
+    //adds an enQVal to the front of the deque
     public void addFirst( T enQVal ) ;
 
-    //adds an enQVal to the end of the queue
+    //adds an enQVal to the end of the deque
     public void addLast( T enQVal ) ;
 
-    //removes and returns the first value of the queue
+    //removes and returns the first value of the deque
     public T removeFirst() ;
 
-    //removes and returns the last value of the queue
+    //removes and returns the last value of the deque
     public T removeLast() ;
 
-    //retrieves the first value of the queue but doesn't remove it
+    //retrieves the first value of the deque but doesn't remove it
     public T peekFirst() ;
 
-    //retrieves the last value of the queue but doesn't remove it
+    //retrieves the last value of the deque but doesn't remove it
     public T peekLast() ;
 
-    //returns true if there are elements in the queue
+    //returns true if there are elements in the deque
     //returns false otherwise
     public boolean isEmpty();
     
